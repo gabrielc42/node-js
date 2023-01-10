85c23d669200ba0883d77f539b6502af8af9416f
gabrielc42 <clark.gabe97@gmail.com>
2023-01-09 18:48:13 -0600

    animal of the day node

--- intro/index.js
+++ intro/index.js
@@ -1,1 +1,12 @@
+//jshint esversion:6
+
 console.log("We meet again world. Hi :D");
+const fs  = require("fs");
+
+fs.copyFileSync("file1.txt", "file2.txt");
+
+const botanicZoo = require("botanic-zoo-api");
+botanicZoo
+  .getAnimalOfTheDay()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
