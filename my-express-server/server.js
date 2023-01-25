bb2cf928ad5112088328b4f081a38be14181b936
gabrielc42 <clark.gabe97@gmail.com>
2023-01-24 22:37:58 -0600

    npm init express server

---
+++ my-express-server/server.js
@@ -0,0 +1,13 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(request, response) {
+  response.send("<h1>hiya</h1>");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
