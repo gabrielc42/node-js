7e9cf300be6fc80bf1ef0f7d38765d65a2021d85
gabrielc42 <clark.gabe97@gmail.com>
2023-04-17 13:27:11 -0500

    body parser init

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,11 +2,16 @@
 
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const calculator = express();
 
 calculator.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
+});
+
+calculator.post("/", function (req, res) {
+  res.send("Thanks for posting that!");
 });
 
 calculator.listen(3000, function () {
