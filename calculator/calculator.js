5fec349a41908b1935629bd1bea7e38226fac9d3
gabrielc42 <clark.gabe97@gmail.com>
2023-04-18 11:27:59 -0500

    processing calculated post requests w/ body-parser

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,12 +6,19 @@ const bodyParser = require("body-parser");
 
 const calculator = express();
 
+calculator.use(bodyParser.urlencoded({ extended: true }));
+
 calculator.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
 calculator.post("/", function (req, res) {
-  res.send("Thanks for posting that!");
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+
+  var result = num1 + num2;
+
+  res.send("The result of the calculation is " + result);
 });
 
 calculator.listen(3000, function () {
