75e45efa8e46fbc0a772276205ea375f5317524d
gabrielc42 <clark.gabe97@gmail.com>
2023-04-18 11:34:27 -0500

    bmi calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,6 +21,18 @@ calculator.post("/", function (req, res) {
   res.send("The result of the calculation is " + result);
 });
 
+calculator.get('/bmiCalculator', function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+calculator.post("/bmiCalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight / (height * height);
+  res.send("BMI calculated: " + bmi);
+});
+
 calculator.listen(3000, function () {
   console.log("Server started on port 3000");
 });
